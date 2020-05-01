15aab8341a8f6a99833bb0263644c49bd79c2893
isso0424 <isso04240129@gmail.com>
2020-05-01 09:48:38 +0900

    [Add]課題お知らせTimeを実装

--- main.go
+++ main.go
@@ -9,12 +9,14 @@ import (
 	"os/signal"
 	"strings"
 	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 var db *sql.DB
+var notifyChannel = "574884574778359844"
 
 func main() {
 	tmp, err := sql.Open("sqlite3", "./db.sqlite3")
@@ -43,7 +45,54 @@ func main() {
 
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	tc := time.NewTicker(time.Second * 10)
+
+	loopContinue := true
+
+	for loopContinue {
+		select {
+		case <-sc:
+			loopContinue = false
+		case <-tc.C:
+			taskNotify(discord)
+		}
+	}
 	<-sc
+}
+
+func taskNotify(session *discordgo.Session) {
+	session.ChannelMessageSend(notifyChannel, "***課題お知らせTIME***")
+	now := getDate(time.Now())
+	session.ChannelMessageSend(notifyChannel, "今日提出期限の課題は以下のとおりです")
+	getTaskWithLimit(now, session)
+	tomorrow := getDate(time.Now().Add(time.Duration(24) * time.Hour))
+	session.ChannelMessageSend(notifyChannel, "明日提出期限の課題は以下のとおりです")
+	getTaskWithLimit(tomorrow, session)
+}
+
+func getDate(date time.Time) string {
+	return fmt.Sprintf("%d-%d-%d", date.Year(), int(date.Month()), date.Day())
+}
+
+func getTaskWithLimit(limit string, session *discordgo.Session) {
+	rows, err := db.Query(`SELECT * FROM TASKS WHERE "LIMIT"=?`, limit)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for rows.Next() {
+		var id int
+		var task string
+		var limit string
+		var subject string
+
+		if err := rows.Scan(&id, &task, &limit, &subject); err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		session.ChannelMessageSend(notifyChannel, fmt.Sprintf("```name: %s\nsubject: %s```", task, subject))
+	}
 }
 
 func createFirstTable() {
