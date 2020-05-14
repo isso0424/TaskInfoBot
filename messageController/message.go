8a01f5393204cc0b219efd41b0387851b1fb6ece
isso0424 <isso04240129@gmail.com>
2020-05-14 23:12:20 +0900

    [Add]hibikinessを降臨させた

--- messageController/message.go
+++ messageController/message.go
@@ -41,23 +41,23 @@ type RemoveError struct {
 
 // CreateTaskAddMessage is a function that creates a message when taskAdd succeeds.
 func CreateTaskAddMessage() (addMessageTemplate string) {
-	addMessageTemplate = "```name: %s\nlimit: %d/%d\nsubject: %s```\nで新しい課題を作成しました。"
+	addMessageTemplate = "```name: %s\nlimit: %d/%d\nsubject: %s```\nで新しい課題を作成したよ。"
 	return
 }
 
 // CreateAddErrorMessage is a function that creates error messages while taskAdd.
 func CreateAddErrorMessage() (addErrors AddError) {
-	addErrors.NotEnoughArgs = "引数が足りません\n最低でも2個は必要です"
-	addErrors.InvalidChannel = "課題を登録する際は<#%s>で行ってください"
-	addErrors.InvalidDatePatarn = "日付の指定は n/m で指定してください"
-	addErrors.InvalidSubjectName = "データの作成に失敗しました\n有効な教科の名前を指定してください"
-	addErrors.DuplicateName = "データの作成に失敗しました\n課題の名前の重複などが無いか確認してください"
+	addErrors.NotEnoughArgs = "引数が足りないよ。\n最低でも課題と教科の名前を指定してね。"
+	addErrors.InvalidChannel = "課題を登録する際は<#%s>で入力してね。"
+	addErrors.InvalidDatePatarn = "日付の指定は 月/日 で指定してね。"
+	addErrors.InvalidSubjectName = "データの作成に失敗したよ。\n有効な教科の名前を指定してね。"
+	addErrors.DuplicateName = "データの作成に失敗したよ。\n課題の名前の重複などが無いか確認してね。"
 	return
 }
 
 // CreateTaskHelpMessage is a function that creates a message when execute taskHelp
 func CreateTaskHelpMessage() (helpMessages HelpMessage) {
-	helpMessages.General = "***課題管理BOT***\n```!task add <task> <limit> <subject>```\ntask: 課題名\nlimit: 締め切り(初期値=翌日)\nsubject: 教科(初期値='')\n教科は省略できる\n```!task list <subject>```\n課題一覧を表示します\n<subject>を指定すると教科ごとの絞り込みが可能です\n```!task remove <task>```\n課題を課題名から検索して削除します```!task help (subject)```\n使い方を表示します\nsubjectを付けると利用可能な教科を表示します"
+	helpMessages.General = "***課題管理BOT***\n```!task add <task> <limit> <subject>```\ntask: 課題名\nlimit: 締め切り(初期値=翌日)\nsubject: 教科(初期値='')\n教科は省略するよ\n```!task list <subject>```\n課題一覧を表示するよ\n<subject>を指定すると教科ごとの絞り込みが可能だよ\n```!task remove <task>```\n課題を課題名から検索して削除するよ```!task help (subject)```\n使い方を表示するよ\nsubjectを付けると利用可能な教科を表示するよ"
 	helpMessages.Subjects = subjectsTemplate{
 		EachSubject: ", %s",
 		EachCourse:  "%s\n```%s```\n",
@@ -73,28 +73,28 @@ func CreateTaskListMessageTemplate() (listMessageTemplate string) {
 
 // CreateListErrorMessage is a function that creates error messages while show task list
 func CreateListErrorMessage() (listErr ListError) {
-	listErr.NotCheckTaskChannel = "このチャンネルは課題確認用に設定されていません"
-	listErr.SubjectIsNotInCourse = "指定された教科は現在のチャンネルの系に存在しません"
-	listErr.GetValueError = "値の取り出しでエラーが発生しました"
-	listErr.TaskNotFound = "このチャンネル向けに作成された課題はありません"
+	listErr.NotCheckTaskChannel = "このチャンネルは課題確認用に設定されてないよ。"
+	listErr.SubjectIsNotInCourse = "指定された教科は現在のチャンネルの系にないよ。"
+	listErr.GetValueError = "値の取り出しでエラーが発生したよ。"
+	listErr.TaskNotFound = "このチャンネル向けに作成された課題は無いよ。"
 	return
 }
 
 // CreateTaskRemoveMessage is a function that creates messsages when removeTask succeeds
 func CreateTaskRemoveMessage() (removeSuccess string) {
-	removeSuccess = "%sを削除しました"
+	removeSuccess = "%sを削除したよ。"
 	return
 }
 
 // CreateRemoveErrorMessage is a function that creates error messages while execute removeTask
 func CreateRemoveErrorMessage() (removeErrors RemoveError) {
-	removeErrors.NotEnoughArgs = "引数が足りません\n削除する課題の名前を指定してください"
-	removeErrors.TaskNotFound = "指定された名前の課題は存在しません"
+	removeErrors.NotEnoughArgs = "引数が足りないよ。\n削除する課題の名前を指定しないよ。"
+	removeErrors.TaskNotFound = "指定された名前の課題は存在しないよ。"
 	return
 }
 
 // CreateGeneralErrorMessage is a function that creates error messages while root of taskManager
 func CreateGeneralErrorMessage() (generalErrors GeneralError) {
-	generalErrors.NotSetupped = "授業一覧が登録されていません"
+	generalErrors.NotSetupped = "授業一覧が登録されてないよ。"
 	return
 }
